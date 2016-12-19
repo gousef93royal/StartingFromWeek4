eb524ecd5485b8bb14e8f4a0c418dca84b3a8f1c
Mohsen Mahmoud Galal <devmohsengalal@gmail.com>
2016-12-19 18:09:38 +0200

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -7,10 +7,36 @@ angular.module('myApp', [
     'Employees',
     'taskOfEmp',
     'dashboardModule',
-  'myApp.version'
+  'myApp.version',
+    'ui.bootstrap'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   //$routeProvider.otherwise({redirectTo: '/dep'});
+}]).
+ controller ('TabsCtrl',['$scope','$location',function ($scope, $location) {
+    $scope.tabs = [
+        { link : '#!/task', label : 'Task Module' },
+        { link : '#!/dep', label : 'Departments Module' },
+        { link : '#!/emp', label : 'Employees Module' },
+        { link : '#!/dashboard', label : 'Dashboard Module' }
+    ];
+
+    $scope.selectedTab = $scope.tabs[0];
+    $scope.setSelectedTab = function(tab) {
+        $scope.selectedTab = tab;
+    }
+
+    $scope.tabClass = function(tab) {
+        if ($scope.selectedTab == tab) {
+            return "active";
+        } else {
+            return "";
+        }
+    }
 }]);
+
+// TabsCtrl($scope, $location) {
+
+// }
